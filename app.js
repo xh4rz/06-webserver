5820fa7cbf316c6037ae80938b306f709945976b
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-05-05 22:08:28 -0500

    91. Servir un sitio web completo

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ const port = 8080;
 // Sevir contenido estático
 app.use(express.static('public'));
 
-app.get('/hola-mundo', (req, res) => {
-	res.send('Hola mundo en su respectiva ruta');
+// app.get('/hola-mundo', (req, res) => {
+// 	res.send('Hola mundo en su respectiva ruta');
+// });
+
+// app.get('*', (req, res) => {
+// 	res.sendFile(__dirname + '/public/404.html');
+// });
+
+app.get('/generic', (req, res) => {
+	res.sendFile(__dirname + '/public/generic.html');
 });
 
-app.get('*', (req, res) => {
-	res.sendFile(__dirname + '/public/404.html');
+app.get('/elements', (req, res) => {
+	res.sendFile(__dirname + '/public/elements.html');
 });
 
 app.listen(port, () => {
