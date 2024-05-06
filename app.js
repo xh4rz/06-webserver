94c9951a006920b5b127e848feb7c923b6be406b
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-05-05 21:26:23 -0500

    88. Request y Response

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const http = require('http');
 
 http
 	.createServer((req, res) => {
-		res.write('Hola Mundo');
+		res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+		res.writeHead(200, { 'Content-Type': 'application/csv' });
+
+		res.write('id, nombre\n');
+		res.write('1, Fernando\n');
+		res.write('2, Maria\n');
+		res.write('3, Juan\n');
+		res.write('4, Pedro\n');
 		res.end();
 	})
 	.listen(8080);
