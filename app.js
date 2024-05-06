c3e7be23e7db34cde645d31ab10d24f83f16b0c0
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-05-05 21:38:17 -0500

    89. Introducción a EXPRESS

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-http
-	.createServer((req, res) => {
-		res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
-		res.writeHead(200, { 'Content-Type': 'application/csv' });
+app.get('/', (req, res) => {
+	res.send('Home page');
+});
 
-		res.write('id, nombre\n');
-		res.write('1, Fernando\n');
-		res.write('2, Maria\n');
-		res.write('3, Juan\n');
-		res.write('4, Pedro\n');
-		res.end();
-	})
-	.listen(8080);
+app.get('/hola-mundo', (req, res) => {
+	res.send('Hola mundo en su respectiva ruta');
+});
 
-console.log('Escuchando el puerto', 8080);
+app.get('*', (req, res) => {
+	res.send('404 | Page not found');
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
