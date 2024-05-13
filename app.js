0795170d4da18ee153befaf5994cbf4a0e22e3d1
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-05-13 00:49:13 -0500

    94. Usando parciales con HBS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 const port = 8080;
 
-// Todo: require('hbs')
+// Handlebars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Sevir contenido estático
 app.use(express.static('public'));
@@ -16,11 +18,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/generic', (req, res) => {
-	res.sendFile(__dirname + '/public/generic.html');
+	res.render('generic', {
+		nombre: 'Harold Gonzalez',
+		titulo: 'Curso de Node'
+	});
 });
 
 app.get('/elements', (req, res) => {
-	res.sendFile(__dirname + '/public/elements.html');
+	res.render('elements', {
+		nombre: 'Harold Gonzalez',
+		titulo: 'Curso de Node'
+	});
 });
 
 app.listen(port, () => {
